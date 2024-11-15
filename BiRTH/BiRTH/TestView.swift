eb0a7999fbd5398ffd9ddbd046c5192c144f59b5
soy <teabag759@gmail.com>
2024-11-15 17:42:02 +0900

    test

---
+++ BiRTH/BiRTH/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  BiRTH
+//
+//  Created by 이소현 on 11/15/24.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestView()
+}
