bf32ac0f88c3385ae322bc1d7d58542c22087cc5
bohemio1234 <bohemio1234@naver.com>
2024-11-15 17:11:54 +0900

    gitignore DSStore 추가

---
+++ BiRTH/BiRTH/BiRTHApp.swift
@@ -0,0 +1,20 @@
+//
+//  BiRTHApp.swift
+//  BiRTH
+//
+//  Created by chanu on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BiRTHApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
