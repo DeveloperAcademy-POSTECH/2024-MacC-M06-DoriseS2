62f1260d7e76648c73a3faf68be59f0f54b35343
cho2-0923 <cho2-@naver.com>
2024-11-23 15:04:36 +0900

    Custom Shape 2개 추가

---
+++ BiRTH/BiRTH/View/FriendListView/FriendListView.swift
@@ -0,0 +1,18 @@
+//
+//  FriendListView.swift
+//  BiRTH
+//
+//  Created by Cho YeonJi on 11/21/24.
+//
+
+import SwiftUI
+
+struct FriendListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FriendListView()
+}
