5d93fa773397b33776655e68127e3ba293dd40af
cho2-0923 <cho2-@naver.com>
2024-11-24 15:02:57 +0900

    CustomShape, SearchBar, BdayAddButton

--- BiRTH/BiRTH/View/FriendListView/SearchBarForFriendListView/SearchBarForFriendListView.swift
+++ BiRTH/BiRTH/View/FriendListView/SearchBarForFriendListView/SearchBarForFriendListView.swift
@@ -8,11 +8,48 @@
 import SwiftUI
 
 struct SearchBarForFriendListView: View {
+    @Binding var text: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            TextField("친구를 찾아보세요", text: $text)
+                .textFieldStyle(SearchBarStyleForFriendListView())
+                .padding(.leading, 16)
+            
+            Button {
+                print("Add profiles")
+            } label: {
+                Image("AddButton")
+                    .resizable()
+                    .frame(width: 46, height: 47)
+            }
+            .padding(.trailing, 16)
+        } //: HSATCK
+    }
+}
+
+// MARK: TextFieldCustomForSearchBar
+struct SearchBarStyleForFriendListView: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 90)
+                .frame(width: 288, height: 45)
+                .foregroundColor(Color.gray.opacity(0.2))
+            
+            HStack {
+                Text(Image(systemName: "magnifyingglass"))
+                    .foregroundColor(Color.black)
+                    .font(.system(size: 24))
+                
+                configuration
+                    .font(.system(size: 12, weight: .regular))
+            }
+            .padding(.leading, 31)
+        }
     }
 }
 
 #Preview {
-    SearchBarForFriendListView()
+    @Previewable @State var searchText = ""
+    SearchBarForFriendListView(text: $searchText)
 }
