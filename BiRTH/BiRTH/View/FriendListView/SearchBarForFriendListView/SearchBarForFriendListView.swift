b93ee4b9388aaa171e669cf2721785fc2cb31efa
cho2-0923 <cho2-@naver.com>
2024-11-24 15:01:43 +0900

    SearchBar and AddBday Button (FriendListViewHeader)

---
+++ BiRTH/BiRTH/View/FriendListView/SearchBarForFriendListView/SearchBarForFriendListView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBarForFriendListView.swift
+//  BiRTH
+//
+//  Created by Cho YeonJi on 11/24/24.
+//
+
+import SwiftUI
+
+struct SearchBarForFriendListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBarForFriendListView()
+}
